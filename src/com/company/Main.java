fc63d0eeab0fd067f1f7d2d66c09cd0b75c4b403
s464895 <antpaz@.st.amu.edu.pl>
2021-10-11 14:36:25 +0200

    javc

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ import java.util.logging.Logger;
 public class Main {
 
     public static void main(String[] args) {
+        int f;
         Logger log = Logger.getLogger("name");
 	    System.out.println("poluczilo");
         log.info("message");
