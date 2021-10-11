7634b2eb6cf34049e3b532de2e06ee8216a65270
s464895 <antpaz@.st.amu.edu.pl>
2021-10-11 14:13:39 +0200

    ja

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import java.util.logging.Logger;
+
 public class Main {
 
     public static void main(String[] args) {
+        Logger log = Logger.getLogger("name");
 	    System.out.println("poluczilo");
+        log.info("message");
     }
 }
